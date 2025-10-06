5bf3ef9500bc64c33a97c2b068907bea7b0d1133
hugopereira-cs <hugoper_cs@yahoo.com>
2025-10-06 15:12:05 -0300

    adiciona projeto do game Yu-Gi-Oh, ainda em construção

---
+++ src/scripts/engine.js
@@ -0,0 +1,134 @@
+// criando um objeto que guardará outros objetos
+const state = {
+    score: {
+        playerScore: 0,
+        computerScore: 0,
+        scoreBox: document.getElementById('score_points'),
+    },
+    cardSprites: {
+        avatar: document.getElementById('card-image'),
+        name: document.getElementById('card-name'),
+        type: document.getElementById('card-type'),
+    },
+    fieldCards: {
+        player: document.getElementById('pleyar-field-card'),
+        computer: document.getElementById('computer-field-card'),
+    },
+    playerSides: {
+        player1: "player-cards",
+        player1BOX: document.querySelector("#player-cards"),
+        computer: "computer-cards",
+        computerBOX: document.querySelector("#computer-cards")
+    },
+    actions: {
+        button: document.getElementById('next-duel'),
+    },
+};
+
+
+const pathImages = './src/assets/icons/';
+
+// enumeração das cartas
+const cardData = [
+    {
+        id: 0,
+        name: 'Blue Eyes White Dragon',
+        type: 'Paper',
+        img: `${pathImages}dragon.png`,
+        WinOf: [1],
+        LoseOf: [2],
+    },
+    {
+        id: 1,
+        name: 'Dark Magician',
+        type: 'Rock',
+        img: `${pathImages}magician.png`,
+        WinOf: [2],
+        LoseOf: [0],
+    },
+    {
+        id: 2,
+        name: 'Exodia',
+        type: 'Scissors',
+        img: `${pathImages}exodia.png`,
+        WinOf: [0],
+        LoseOf: [1],
+    },
+];
+
+// gera um ID aleatório, e o arredonda para baixo (Math.floor) para evitar números quebrados
+async function getRandomCardId() {
+    const randomIndex = Math.floor(Math.random() * cardData.length);
+    return cardData[randomIndex].id;
+}
+
+async function createCardImage(IdCard, fieldSide) {
+    const cardImage = document.createElement('img');
+    cardImage.setAttribute('height', '100px');
+    cardImage.setAttribute('src', './src/assets/icons/card-back.png');
+    cardImage.setAttribute('data-id', IdCard); // "data" permite criar atributos dinâmicos, neste caso, id
+    cardImage.classList.add('card');
+
+    if (fieldSide === playerSides.player1) {
+        cardImage.addEventListener('mouseover', () => {
+            drawSelectCard(IdCard);
+        });
+
+        cardImage.addEventListener('click', () => {
+            setCardsField(cardImage.getAttribute('data-id'));
+        });
+    }
+
+
+    return cardImage;
+}
+
+async function  setCardsField(cardId) {
+    await removeAllCardsImages();
+
+    let computerCardId = await getRandomCardId();
+
+    state.fieldCards.player.style.display = "block";
+    state.fieldCards.computer.style.display = "block";
+
+    state.fieldCards.player.src = cardData[cardId].img;
+    state.fieldCards.computer.src = cardData[computerCardId].img;
+
+    let duelResults = await checkDuelResults(cardId, computerCardId);
+
+//     await updateScore();
+//     await drawButton(duelResults);
+
+}
+
+async function removeAllCardsImages() {
+    let { computerBOX, player1BOX } = state.playerSides;
+    let imgElements = cards.querySelectorAll("img");
+    imgElements.forEach((ims) => img.remove());
+    
+    imgElements = cards.querySelectorAll("img");
+    imgElements.forEach((ims) => img.remove());
+}
+
+async function drawSelectCard(index) {
+    state.cardSprites.avatar.src = cardData[index].img;
+    state.cardSprites.name.innerText = cardData[index].name;
+    state.cardSprites.type.innerText = "Atribute : " + cardData[index].type
+}
+
+async function drawCards(cardNumbers, fieldSide) {
+    for (let i = 0; i < cardNumbers; i++) {
+        const randomIdCard = await getRandomCardId();
+        const cardImage = await createCardImage(randomIdCard, fieldSide);
+
+        document.getElementById(fieldSide).appendChild(cardImage);
+    }
+}
+
+// função principal
+function init() {
+    drawCards(5, playerSides.player1);
+    drawCards(5, playerSides.computer);
+}
+
+init();
